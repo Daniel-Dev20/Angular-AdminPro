c81826e258b4e38042bdf29fbd6af9eacb0ec926
Daniel <arevalodev20@gmail.com>
2021-10-03 18:53:53 -0600

    final seccion3 -diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcumbsComponent } from './shared/breadcumbs/breadcumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    Page404Component,
+    DashboardComponent,
+    BreadcumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
